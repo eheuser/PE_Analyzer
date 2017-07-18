185f61a7027d7349c965bd8d14b01e0d2ec73ec0
erik <erik.heuser@gmail.com>
2017-07-18 09:34:34 -0400

    spelling is hard

--- PE_Analyzer.py
+++ PE_Analyzer.py
@@ -223,7 +223,7 @@ This script will extract static details from a valid PE
 and print them out.  Items analyzed:
 -Hashes
 -Import, Exports and Import hash
--Static details such sa compile time and version information
+-Static details such as compile time and version information
 -Fuzzy Hashes for PE, Imports and Sections
 -Cleartext and XOR encoded strings of interest
 -Cleartext and XOR encoded PE's embedded within sample 
